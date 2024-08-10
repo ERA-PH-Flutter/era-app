17e392ff1bf4b4f34de093d645c94c4f8cfb52b6
niiko <nrcapiroso@gmail.com>
2024-08-10 20:52:15 +0800

    a

--- lib/presentation/base/controllers/base_controller.dart
+++ lib/presentation/base/controllers/base_controller.dart
@@ -5,8 +5,14 @@ enum BaseState{
   loading,
   loaded,
   error,
+  empty
 }
 class BaseController extends GetxController{
   var store = Get.find<LocalStorageService>();
-
+  var baseState = BaseState.loading.obs;
+  @override
+  void onInit() {
+    baseState.value = BaseState.loaded;
+    super.onInit();
+  }
 }
--- lib/presentation/base/pages/base.dart
+++ lib/presentation/base/pages/base.dart
@@ -1,14 +1,50 @@
-import 'package:eraphilippines/presentation/home/controllers/home_controller.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:eraphilippines/app/constants/colors.dart';
+import 'package:eraphilippines/presentation/base/controllers/base_controller.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+//todo add text
 
-class Base extends GetView<HomeController>{
+class Base extends GetView<BaseController>{
   const Base({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: WillPopScope(
+        onWillPop: (){
+          Get.back();
+          return Future.value(false);
+        },
+        child: SafeArea(
+          child: Obx(()=>switch(controller.baseState.value){
+            BaseState.loading => _loading(),
+            BaseState.loaded => _loaded(),
+            BaseState.error => _error(),
+            BaseState.empty => _empty()
+          }),
+        ),
+      ),
+    );
   }
+  _loading(){
+    return Center(
+      child: CircularProgressIndicator(
+        color: AppColors.primary,
+      ),
+    );
+  }
+  _loaded(){
+    return Column(
+      children: [
 
+      ],
+    );
+  }
+  _error(){
+    //todo add error screen
+  }
+  _empty(){
+    //todo add empty screen
+  }
 }
