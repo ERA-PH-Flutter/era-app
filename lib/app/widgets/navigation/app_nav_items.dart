15b37acb9d7087f6a4f372360fb9f70d499ce981
beaver <ian418483@gmail.com>
2024-11-12 20:55:54 +0800

    revert not good navigation

--- lib/app/widgets/navigation/app_nav_items.dart
+++ lib/app/widgets/navigation/app_nav_items.dart
@@ -1,5 +1,11 @@
+import 'package:eraphilippines/presentation/agent/agents/controllers/agents_binding.dart';
+import 'package:eraphilippines/presentation/agent/home/controllers/home_binding.dart';
+import 'package:eraphilippines/presentation/agent/listings/searchresult/controllers/searchresult_binding.dart';
+import 'package:eraphilippines/presentation/agent/projects/controllers/projects_binding.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import '../../../presentation/global.dart';
 import 'customenavigationbar.dart';
 
@@ -35,37 +41,32 @@ class AppNavItems extends StatelessWidget {
     } else {
       return GestureDetector(
         onTap: index != null
-            ? () async {
+            ? () {
                 if (index == 1) {
                   selectedIndex.value = 0;
-                  pageViewController.animateToPage(0,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut);
+                  pageViewController = PageController(initialPage: 0);
                   currentRoute = '/home';
+                  Get.offAll(BaseScaffold(), binding: HomeBinding());
                 } else if (index == 2) {
                   selectedIndex.value = 1;
                   currentRoute = '/project-main';
-                  pageViewController.animateToPage(1,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut);
+                  pageViewController = PageController(initialPage: 1);
+                  Get.offAll(BaseScaffold(), binding: ProjectsBinding());
                 } else if (index == 3) {
                   selectedIndex.value = 2;
                   currentRoute = '/searchresult';
-                  pageViewController.animateToPage(2,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut);
+                  pageViewController = PageController(initialPage: 2);
+                  Get.offAll(BaseScaffold(), binding: SearchResultBinding());
                 } else if (index == 4) {
                   selectedIndex.value = 3;
                   currentRoute = '/findagents';
-                  pageViewController.animateToPage(3,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut);
+                  pageViewController = PageController(initialPage: 3);
+                  Get.offAll(BaseScaffold(), binding: AgentsBinding());
                 } else if (index == 5) {
                   selectedIndex.value = 4;
                   currentRoute = '/help';
-                  pageViewController.animateToPage(4,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut);
+                  pageViewController = PageController(initialPage: 4);
+                  Get.offAll(BaseScaffold());
                 }
               }
             : null,
