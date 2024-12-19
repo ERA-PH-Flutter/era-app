afce4d65bce085dbfee704996557edab2fb578bc
xoxolrjj <xwonzws@gmail.com>
2024-12-19 19:27:29 +0800

    find agents fix ui

--- lib/app/widgets/listings_web/agentlistview.dart
+++ lib/app/widgets/listings_web/agentlistview.dart
@@ -15,6 +15,7 @@ import '../button.dart';
 
 class AgentListViewWeb extends StatelessWidget {
   final List agentInfo;
+
   const AgentListViewWeb({super.key, required this.agentInfo});
 
   @override
@@ -23,6 +24,8 @@ class AgentListViewWeb extends StatelessWidget {
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
         mainAxisExtent: Get.height - 350.h,
+        crossAxisSpacing: 20.w,
+        mainAxisSpacing: 20.h,
       ),
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
@@ -37,152 +40,149 @@ class AgentListViewWeb extends StatelessWidget {
                 'mailto:${agent.email}?subject=Your%20Subject&body=Your%20Message')
             : Uri.parse('https://mail.google.com/');
         return Container(
-          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(horizontal: 15.w),
           child: Stack(
             clipBehavior: Clip.none,
             children: [
+              // Card Container
               Container(
-                  height: 400.h,
-                  width: 400
-                      .w, // margin: EdgeInsets.only(top: 120.h, left: 60.w, right: 60.w),
-                  margin: EdgeInsets.only(
-                    top: 250.h,
-                    left: 45.w,
-                    right: 45.w,
+                height: 450.h,
+                width: 450.w,
+                margin: EdgeInsets.only(top: 200.h),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      offset: Offset(0, 5),
+                      blurRadius: 10,
+                    ),
+                  ],
+                  border: Border.all(
+                    color: AppColors.hint.withOpacity(0.3),
+                    width: 1.5,
                   ),
-                  padding: EdgeInsets.only(left: 15.w, right: 15.w),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: AppColors.hint.withOpacity(0.5), width: 3.w),
-                      borderRadius: BorderRadius.circular(8)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
                   child: Column(
                     children: [
-                      SizedBox(height: 80.h),
+                      SizedBox(height: 100.h),
                       EraText(
                         text: '${agent.firstname} ${agent.lastname}',
-                        fontSize: 25.sp,
+                        fontSize: EraTheme.h3,
                         fontWeight: FontWeight.w600,
-                        lineHeight: 1.0,
-                        textOverflow: TextOverflow.ellipsis,
+                        lineHeight: 1.2,
                         textAlign: TextAlign.center,
                         color: AppColors.blue,
                       ),
                       EraText(
                         text:
                             '${agent.role == 'agent' ? 'ERA Infinity Agent' : 'ERA Infinity Broker'}',
-                        fontSize: 15.sp,
+                        fontSize: EraTheme.h5,
                         fontWeight: FontWeight.w400,
-                        color: AppColors.black,
-                        textOverflow: TextOverflow.ellipsis,
+                        color: AppColors.black.withOpacity(0.8),
                         textAlign: TextAlign.center,
                       ),
-                      sb10(),
+                      sb20(),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(whatsAppUrl2);
-                            },
-                            child: Container(
-                              padding: EdgeInsets.only(
-                                  left: 12.w,
-                                  right: 12.w,
-                                  top: 12.h,
-                                  bottom: 12.h),
-                              decoration: BoxDecoration(
-                                  color: AppColors.subtle,
-                                  borderRadius: BorderRadius.circular(30)),
-                              child: Row(
-                                children: [
-                                  Image.asset(
-                                    AppEraAssets.whatsappIcon,
-                                    width: 40.w,
-                                    height: 40.h,
-                                  ),
-                                  sbw5(),
-                                  EraText(
-                                    text: '${agent.whatsApp}',
-                                    fontSize: 12.sp,
-                                    fontWeight: FontWeight.bold,
-                                    color: AppColors.black,
-                                    textOverflow: TextOverflow.ellipsis,
-                                    maxLines: 3,
-                                  ),
-                                ],
-                              ),
-                            ),
+                          _buildContactButton(
+                            onTap: () => launchUrl(whatsAppUrl2),
+                            icon: AppEraAssets.whatsappIcon,
+                            text: '${agent.whatsApp}',
                           ),
                           sb10(),
-                          GestureDetector(
+                          _buildContactButton(
                             onTap: () => launchUrl(emailUrl),
-                            child: Container(
-                              padding: EdgeInsets.only(
-                                  left: 12.w,
-                                  right: 12.w,
-                                  top: 12.h,
-                                  bottom: 12.h),
-                              decoration: BoxDecoration(
-                                  color: AppColors.subtle,
-                                  borderRadius: BorderRadius.circular(30)),
-                              child: Row(
-                                children: [
-                                  Image.asset(
-                                    //  color: AppColors.kRedColor,
-                                    AppEraAssets.emailIcon,
-                                    width: 40.w,
-                                    height: 40.h,
-                                  ),
-                                  sbw5(),
-                                  Container(
-                                    width: 190.w,
-                                    child: EraText(
-                                      text: '${agent.email}',
-                                      fontSize: 12.sp,
-                                      fontWeight: FontWeight.bold,
-                                      color: AppColors.black,
-                                      textOverflow: TextOverflow.ellipsis,
-                                      maxLines: 3,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                            icon: AppEraAssets.emailIcon,
+                            text: '${agent.email}',
                           ),
                         ],
                       ),
-                      sb40(),
+                      Spacer(),
                       Button(
                         text: 'VIEW LISTING',
-                        fontSize: 13.sp,
+                        fontSize: 14.sp,
                         onTap: () {
                           // Get.to(AgentListings(),
                           //     binding: AgentListingsBinding(),
                           //     arguments: [agent.id]);
                         },
                         bgColor: AppColors.kRedColor,
-                        width: 200.w,
-                        fontWeight: FontWeight.w400,
-                        margin: EdgeInsets.symmetric(horizontal: 35),
-                        borderRadius: BorderRadius.circular(20),
+                        width: 220.w,
+                        fontWeight: FontWeight.w500,
+                        margin: EdgeInsets.only(bottom: 15.h),
+                        borderRadius: BorderRadius.circular(30),
                       ),
-                      //????
                     ],
-                  )),
+                  ),
+                ),
+              ),
+
               Positioned(
-                  top: 10.h,
-                  left: 100.w,
-                  right: 100.w,
+                top: 0.h,
+                right: 20.w,
+                left: 20.w,
+                child: Align(
+                  alignment: Alignment.topCenter,
                   child: CloudStorage().imageLoaderProvider(
-                      height: 300.h,
-                      width: Get.width,
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      reference:
-                          '${agent.image ?? AppStrings.noUserImageWhite}')),
+                    height: 280.h,
+                    width: 300.w,
+                    reference: '${agent.image ?? AppStrings.noUserImageWhite}',
+                  ),
+                ),
+              ),
             ],
           ),
         );
       },
+    );
+  }
+
+  Widget _buildContactButton({
+    required VoidCallback onTap,
+    required String icon,
+    required String text,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 15.w),
+        decoration: BoxDecoration(
+          color: AppColors.subtle,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              blurRadius: 8,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              icon,
+              width: 50.w,
+              height: 50.h,
+            ),
+            sbw10(),
+            Expanded(
+              child: EraText(
+                text: text,
+                fontSize: EraTheme.h5,
+                fontWeight: FontWeight.bold,
+                color: AppColors.black.withOpacity(0.9),
+                maxLines: 1,
+                textOverflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
