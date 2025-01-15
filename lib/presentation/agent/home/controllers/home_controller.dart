0fe4b35262246f2dc2478855adf6633b09308a34
Nikko <nrcapiroso@gmail.com>
2025-01-16 06:48:47 +0800

    fix blank home

--- lib/presentation/agent/home/controllers/home_controller.dart
+++ lib/presentation/agent/home/controllers/home_controller.dart
@@ -52,7 +52,6 @@ class HomeController extends GetxController {
 
   @override
   void onInit() async {
-<<<<<<< HEAD
     //print(Get.find<LocalStorageService>().images);
     try {
       await getBanners();
@@ -61,23 +60,10 @@ class HomeController extends GetxController {
       await getImages();
       await getListings();
       await getProjects();
-      await Future.delayed(Duration(seconds: 1, milliseconds: 500));
+      //await Future.delayed(Duration(seconds: 1,milliseconds: 500));
       homeState.value = HomeState.loaded;
     } catch (e) {
-      print("errror: $e");
-=======
-    try {
-    await getBanners();
-    quickLinks = await QuickLinksModel().initialize();
-    await getNews();
-    await getImages();
-    await getListings();
-    await getProjects();
-    //await Future.delayed(Duration(seconds: 1,milliseconds: 500));
-    homeState.value = HomeState.loaded;
-    } catch (e) {
       print('result $e');
->>>>>>> 091cb2fad241f430dde986d760624f17225cb634
       homeState.value = HomeState.error;
     }
     super.onInit();
@@ -115,31 +101,26 @@ class HomeController extends GetxController {
   getProjects() async {
     if (settings!.featuredProjects != null) {
       for (int i = 0; i < settings!.featuredProjects!.length; i++) {
-<<<<<<< HEAD
-        try {
-          var pr = await Project.getById(settings!.featuredProjects![i]);
-=======
         var pr = await Project.getById(settings!.featuredProjects![i]);
         if (pr != null) {
->>>>>>> 091cb2fad241f430dde986d760624f17225cb634
-          projects.add(GestureDetector(
-            onTap: () {
-              Get.to(ProjectView(),
-                  binding: ProjectViewBinding(), arguments: pr);
-            },
-            child: Wrap(
-              children: [
-                Column(
-                  children: ProjectViews(project: pr).HomebuildPreview(),
-                ),
-              ],
-            ),
-          ));
-<<<<<<< HEAD
-        } catch (e) {}
-=======
+          try {
+            projects.add(GestureDetector(
+              onTap: () {
+                Get.to(ProjectView(),
+                    binding: ProjectViewBinding(), arguments: pr);
+              },
+              child: Wrap(
+                children: [
+                  Column(
+                    children: ProjectViews(project: pr).HomebuildPreview(),
+                  ),
+                ],
+              ),
+            ));
+          } catch (e) {
+            print('error $e');
+          }
         }
->>>>>>> 091cb2fad241f430dde986d760624f17225cb634
       }
     }
   }
